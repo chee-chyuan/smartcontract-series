f7a8721b7deff00420c8f8539460324fcef747a2
chee-chyuan <60590919+chee-chyuan@users.noreply.github.com>
2021-10-24 22:25:28 +0800

    added contractfactory.sol and added pause and unpause with hook stub

---
+++ 9_minimal_proxy_upgradeable_erc721/contracts/ContractFactoryStorage.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.4;
+
+contract ContractFactoryStorage {
+    //mapping countid to cloneaddress
+    mapping(uint256 => address) private _cloneAddresses;
+    //map to bool true if clone exist
+    mapping(address => bool) private _cloneExist;
+
+    uint256 public totalPaused;
+    uint256 public totalUnpaused;
+
+    //mapping on countId to index of clone in paused/unpaused list
+    mapping(uint256 => uint256) private _pausedCloneIndex;
+    mapping(uint256 => uint256) private _unpausedCloneIndex;
+
+    //mapping of index to clone address in list
+    mapping(uint256 => address) private _pausedList;
+    mapping(uint256 => address) private _unpausedList;
+
+    function _beforePaused(address _cloneAddress) internal {
+        //TODO
+    }
+
+    function _beforeUnpaused(address _cloneAddress) internal {
+        //TODO
+    }
+}
--- 9_minimal_proxy_upgradeable_erc721/contracts/Erc721ContractFactory.sol
+++ 9_minimal_proxy_upgradeable_erc721/contracts/Erc721ContractFactory.sol
@@ -4,8 +4,9 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./interfaces/IErc721Implementation.sol";
+import "./ContractFactoryStorage.sol";
 
-contract Erc721ContractFactory is Ownable {
+contract Erc721ContractFactory is Ownable, ContractFactoryStorage {
     address public implementation;
 
     event CreatedNewCloneContract(address indexed contractAddress);
@@ -21,16 +22,47 @@ contract Erc721ContractFactory is Ownable {
     ) public onlyOwner {
         //clone
         address cloneAddress = Clones.clone(implementation);
+
         //initialize
-        IErc721Implementation(cloneAddress).initialize(_name, _symbol, _maxSupply);
+        (bool success, ) = cloneAddress.call(
+            abi.encodeWithSignature(
+                "initialize(string,string,uint256)",
+                _name,
+                _symbol,
+                _maxSupply
+            )
+        );
+
+        require(success, "clone is unsuccessful");
+
         emit CreatedNewCloneContract(cloneAddress);
     }
 
     function mint(address _to) public {
         //check price
         //if ok we mint
+    }
 
-        
+    function pause(address _cloneAddress) public onlyOwner {
+        _beforePaused(_cloneAddress);
+
+        //pause contract
+        (bool success, ) = _cloneAddress.call(
+            abi.encodeWithSignature("pause()")
+        );
+
+        require(success, "pause is unsuccessful");
+    }
+
+    function unpause(address _cloneAddress) public onlyOwner {
+        _beforeUnpaused(_cloneAddress);
+
+        //unpause contract
+        (bool success, ) = _cloneAddress.call(
+            abi.encodeWithSignature("unpause()")
+        );
+
+        require(success, "unpause is unsuccessful");
     }
 
     function setImplementation(address _implementation) public onlyOwner {
